7a8fbd3e072e351331f838373b07b134287592be
Xiangchun Fu <xfu@redhat.com>
2024-08-19 14:56:14 +0800

    create a new script for trustee setup

---
+++ trustee-setup.sh
@@ -0,0 +1,11 @@
+# git clone https://github.com/lmilleri/trustee-operator-install.git
+# cd trustee-operator-install
+
+
+
+# oc apply -f ns.yaml
+# oc apply -f kbs_catalog-ocp.yaml
+# oc apply -f og.yaml
+# oc apply -f subs-ocp.yaml
+
+
